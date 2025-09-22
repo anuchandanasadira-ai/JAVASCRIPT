9c23ee532e98627e5014cdedcf357960b1bad5ac
anuchandanasadira-ai <anuchandanasadira@gmail.com>
2025-09-22 14:21:18 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,17 @@
+function add() {
+  var a = 60;
+  var b = 70;
+  document.write(a + b);
+  console.log(a + b);
+}
+add();
+const add = (45,67) => console.log(a+b)
+add(57,67);
+let name ="anu";
+function demo();
+{
+  let name ="nandhu"
+  document.write('hello, my name is $(anu)');
+
+demo();
+}
